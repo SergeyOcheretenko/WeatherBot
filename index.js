41c392f5a4d038fc77f3f289e81d913a1fa4eeca
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-04-29 20:28:41 +0300

    feat: add check weather function
    Add checking weather structure and add rule in gitignore.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("I'm working");
+import fetch from 'node-fetch';
+import config from './CONFIG.json' assert {type: "json"};
+
+const API_KEY = config.weather_api;
+
+async function fetchWeather(city) {
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=ua&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+}
+
+fetchWeather('Kiev');
